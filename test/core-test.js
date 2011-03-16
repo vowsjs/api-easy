e279c4ea64f42d48ca267488082c52ab1ab08d36
indexzero <charlie.robbins@gmail.com>
2011-03-16 12:47:22 -0400

    [api test] Move _request() to be "private" & a couple of other small changes

--- test/core-test.js
+++ test/core-test.js
@@ -18,12 +18,36 @@ var scopes = ['When using the Test API', 'the Foo Resource'];
 vows.describe('rest-easy/core').addBatch({
   "When using RESTeasy": {
     topic: restEasy.describe('test/api').discuss('When using the Test API'),
+    "it should have the correct methods set": function () {
+      assert.isFunction(restEasy.describe);
+      assert.length(Object.keys(restEasy), 1);
+    },
     "and a valid suite": {
       topic: function (suite) {
         return suite.discuss('the Foo Resource')
           .use('localhost', 8080)
           .setHeaders({ 'Content-Type': 'application/json' })
           .path('/tests');
+      },
+      "it should have the correct methods set": function (suite) {
+        ['discuss', 'use', 'setHeaders', 'path', 'unpath', 'root', 'get', 'put', 
+          'post', 'del', 'expect', 'next', 'export', '_request', '_currentTest'].forEach(function (key) {
+          assert.isFunction(suite[key]);
+        });
+      },
+      "the path() method": {
+        "should append the path to the suite": function (suite) {
+          var length = suite.paths.length;
+          suite.path('/more-tests');
+          assert.length(suite.paths, length + 1);
+          assert.equal('/more-tests', suite.paths[suite.paths.length - 1]);
+        }
+      },
+      "the unpath() method": {
+        "should remove the path from the suite": function (suite) {
+          var length = suite.paths.length;
+          suite.unpath();
+        }
       },
       "a GET test": {
         "with no path": {
@@ -48,6 +72,18 @@ vows.describe('rest-easy/core').addBatch({
           },
           "should have the correct options": helpers.assertOptions(scopes, 'A GET to /tests/path-test', {
             uri: 'http://localhost:8080/tests/path-test',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            length: 2
+          })
+        },
+        "with an additional path and parameters": {
+          topic: function (suite) {
+            return suite.get('/path-test', { foo: 1, bar: 2 }).expect(200).batch;
+          },
+          "should have the correct options": helpers.assertOptions(scopes, 'A GET to /tests/path-test?foo=1&bar=2', {
+            uri: 'http://localhost:8080/tests/path-test?foo=1&bar=2',
             headers: {
               'Content-Type': 'application/json'
             },
